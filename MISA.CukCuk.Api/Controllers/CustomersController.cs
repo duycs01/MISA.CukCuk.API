4b2acfd00300044dc264de9e9bb45e7d13e9b1a9
duyle <duycs01@gmail.com>
2021-08-07 08:34:21 +0700

    Add project files.

---
+++ MISA.CukCuk.Api/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.CukCuk.Api.Model;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET, POST, PUT, DELETE
+        [HttpGet]
+        
+        [HttpGet]
+        public IActionResult GetCustomers(string customerId)
+        {
+            // Truy cập vào database
+            // 1. Khai báo thông tin database
+            var connectionString = "Host - 47.241.69.179" +
+                "DataBase = MISA.CukCuk_Demo+NVMANH;" +
+                "User Id = dev" +
+                "Passwork = 12345678";
+
+            // 2. Khởi tạo đối tượng kết nối với database
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+
+            // 3. Lấy dữ liệu
+            var sqlCommand = "SELECT * FROM Customer WHERE CustomerId = @CustomerIdParam";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@CustomerIdParam", customerId);
+
+            var customers = dbConnection.QueryFirstOrDefault<Customer>(sqlCommand);
+
+            var response = StatusCode(200, customerId);
+            return response;
+        }
+
+        [HttpPost]
+        public string InsertCustomer([FromBody]List<Customer> customers,[FromHeader]string token)
+        {
+            return token;
+        }
+    }
+}
---
+++ MISA.CukCuk.Api/Model/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Api.Model
+{
+    public class Customer
+    {
+        #region Property
+        /// <summary>
+        /// ID khách hàng
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        /// <summary>
+        /// Mã khách hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+
+        /// <summary>
+        /// Họ khách hàng
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Tên khách hàng
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Họ và tên khách hàng
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Giới tính
+        /// </summary>
+        public int? Gender { get; set; }
+
+        /// <summary>
+        /// Địa chỉ
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Ngày sinh
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        public int MyProperty { get; set; }
+        #endregion
+    }
+}
